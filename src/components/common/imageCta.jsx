44edde53a4f86f46737d81581aac56f71e3dec7e
Guilherme Fernandez <gui.bovo@hotmail.com>
2022-04-16 12:10:41 +0100

    :rocket: Adjust content pages for mobile

--- src/components/common/imageCta.jsx
+++ src/components/common/imageCta.jsx
@@ -2,7 +2,7 @@ const ImageCta = props => {
   return(
     <div className="image-cta">
       <img className="image-wrapper" src={props.image} alt={props.alt}></img>
-      <div clasName="copy">
+      <div id="image-cta-copy" clasName="copy">
         <div className="title">{props.title}</div>
         <div className="description">{props.description}</div>
         <button> {props.buttonText} </button>
