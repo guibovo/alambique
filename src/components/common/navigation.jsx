150e4384f73d05edc64f33dcd93581b637ad3a70
Guilherme Fernandez <gui.bovo@hotmail.com>
2022-04-23 15:36:03 +0100

    :construction: Starts mobile menu

--- src/components/common/navigation.jsx
+++ src/components/common/navigation.jsx
@@ -1,22 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 const Navigation = () => {
   const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   
   useEffect(() => {
     const pathName= location.pathname;
     const ishomeScreen = (pathName === '/' || pathName === '/alambique'  || pathName === '/alambique/');
 
     let element = document.getElementById("headerNavigation");
-    let logoElement = document.getElementById("headerLogo")
+    let logoElement = document.getElementById("headerLogo");
+    let burgerMenu = document.getElementById("burger-menu");
     
     if (ishomeScreen) {
       element.classList.add("white");
       logoElement.classList.remove("black-logo");
+      burgerMenu.classList.remove("black-burger");
     } else {
       element.classList.remove("white");
       logoElement.classList.add("black-logo");
+      burgerMenu.classList.add("black-burger");
     }
     
     window.onscroll = (e) => {
@@ -37,13 +45,19 @@ const Navigation = () => {
     }
     return '';
   }
-  
+
   return(
     <div className="navigation-wrapper">
       <nav id="headerNavigation" className="navigation">
         <Link id="headerLogo" to="/alambique" className="logo">
         </Link>
-        <div className="links">
+        <div id="burger-menu" className={isOpen ? "open" : ""} onClick={() => handleToggleMenu()}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className={`links ${isOpen ? "open" : ""}`}>
           <Link className={underlineCurrentPage('/alambique')} to="/alambique">Home</Link>
           <Link className={underlineCurrentPage('/branding')} to="/branding">Branding</Link>
           <Link className={underlineCurrentPage('/social-media')} to="/social-media">Social Media</Link>
